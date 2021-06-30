606ed9d6f50c24f9753579a55976c26ba6887328
github-solar <solarsun@nate.com>
2021-06-30 11:31:52 +0900

    Default Changelist

--- ch08/file_io/fwrite_list.py
+++ ch08/file_io/fwrite_list.py
@@ -2,18 +2,20 @@
 
 f = open('e:/python/pyworks-jds/pyfile/2021kbo.txt', 'w')
 team = ['기아', '삼성', '쓱', '롯데', '키움']
-'''
-for문으로 리스트 자료를 입력
+
+
+#for문으로 리스트 자료를 입력
 for i in team:
     f.write(i + ' ')
 f.close()
-'''
 
+'''
 # range()함수로 파일에 리스트 자료 입력
 n = len(team)
 for i in range(n):
     f.write(team[i] + ' ')
 f.close()
+'''
 
 f = open('e:/python/pyworks-jds/pyfile/2021kbo.txt', 'r')
 data = f.read()
---
+++ ch08/file_io/pickle2.py
@@ -0,0 +1,11 @@
+#py10일이론02 pickle
+
+import pickle
+#객체의 형태를 그대로 저장하고 불러오는 모듈
+#바이너리 모드 사용
+
+with open('animals.txt', 'wb') as f:
+    li = ['강아', '고양', '양', '말']
+    dic = {1:'사과', 2:'딸기', 3:'수박'}
+    pickle.dump(li, f)
+    pickle.dump(dic, f)
